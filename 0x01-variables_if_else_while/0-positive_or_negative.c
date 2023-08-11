d40af7848ca383415c335cc853e7c5e5cf0b154f
Iyedderar <iyedderar@yahoo.com>
2023-08-11 06:24:55 -0700

    FML

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,12 +10,12 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 if(n>0) 
-printf("%d is positive",&n);
+printf("%ls is positive",&n);
 else {
         if(n<0)
-printf("%d is negative",&n);
+printf("%ls is negative",&n);
 	else{
-printf("%d is zero",&n);
+printf("%ls is zero",&n);
 	}
 }
 return (0);
