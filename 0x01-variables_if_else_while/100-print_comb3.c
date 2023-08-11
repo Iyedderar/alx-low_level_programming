e628ca7895b56bf5a7d6da014d8892c26b3b4569
Iyedderar <iyedderar@yahoo.com>
2023-08-11 16:05:15 -0700

    FML

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+/**
+ * main - print numbers
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int c = '0';
+
+	int j, i = '1';
+
+	for (; c <= '9'; c++)
+	{
+	
+	for (j = i; j <= '9'; j++)
+	{
+		putchar(c);
+		putchar(j);
+		if (c == '8')
+			break;
+		putchar(',');
+		putchar(' ');
+	}
+	i++;
+	}
+	putchar('\n');
+	return (0);
+}
