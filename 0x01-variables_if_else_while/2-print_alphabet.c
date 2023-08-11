9c6c811bd906ae70ba606ac8dbe49f9bd004ca1a
Iyedderar <iyedderar@yahoo.com>
2023-08-11 12:45:38 -0700

    FML

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main - print the alphabet
+ * Return: 0 on success
+
+ */
+int main(void)
+{
+    char c='a';
+
+    while (c!=122)
+    {
+     putchar(c);
+     c++;
+    }
+    putchar('\n');
+    return (0);
+}
