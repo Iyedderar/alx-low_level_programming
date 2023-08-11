e6204422a532a115406fed3c8240506d5bd20aba
Iyedderar <iyedderar@yahoo.com>
2023-08-11 14:07:04 -0700

    FML

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * main - print the alphabet
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int i=0;
+
+	for ( ; i < 10 ; i++)
+		printf("%d",i);
+	putchar('\n');
+	return (0);
+}
