497d65eb317fe7fac4a8180e7ad01108a8c6cd99
Iyedderar <iyedderar@yahoo.com>
2023-08-15 15:08:17 -0700

    FML

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include"main.h"
+/**
+ * print_to_98 - print numbers to 98
+ * @n: starting number
+ * Return: void
+ */
+void add(int n)
+{
+	if (n <= 98)
+        {
+                while(n <= 98)
+                {
+                        printf(n == 98 ? "%d" : "%d, ", n);
+                        ++n;
+                }
+        }
+        else
+        {
+                while(n >= 98)
+                {
+                        printf("%d, ", n);
+                        --n;
+                }
+        }
+        printf("\n");
+}
