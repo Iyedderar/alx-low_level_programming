8ef8dca43b6d6db0ffb4dcf29db434b91505677b
Iyedderar <Iyedderar@yahoo.com>
2023-08-15 17:06:57 +0100

    FML

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_sign - checks the sign of an integer
+ * @n: the integer to be checked
+ * Return: 1 when positif and 0 when zero and -1 when negative
+ */
+
+int print_sign(int n);
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else
+	{
+		if (n < 0)
+		{
+			_putchar('-');
+			return (-1);
+		}
+		else
+		{
+			_putchar('0');
+			return (0);
+		}
+	}
+}
+
