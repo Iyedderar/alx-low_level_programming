07899f4f7ea884b414791f565bb242489971df22
Iyedderar <iyedderar@yahoo.com>
2023-08-15 13:45:30 -0700

    FML

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,9 +2,11 @@
 #include"main.h"
 /**
  * add - add two numbers
+ * @a: First operand
+ * @b: Second operand
  * Return: a + b
  */
-int add(int a,int b)
+int add(int a ,int b)
 {
 	return (a + b);
 }
