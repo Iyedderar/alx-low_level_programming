595a961b7550b9cbb019c0b7e9b9e3c3fb6d2631
Iyedderar <Iyedderar@yahoo.com>
2023-08-15 11:14:45 +0100

    FML

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,10 @@
 #include "main.h"
+
 /**
- * main - print the alphabet except e and q
- * Return: 0 on success
+ * print_alphabet - prints the alphabet in lowercase, followed by a new line.
  */
-void print_alphabet (void)
+
+void print_alphabet(void)
 {
 	char c;
 
