dfbbff5a2eb68658b483c4fe9aea4a4744fa8799
Iyedderar <Iyedderar@yahoo.com>
2023-08-15 18:16:52 +0100

    FML

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit of a number
+ * @n: the integer operated on
+ * Return: the absolute value of the number
+ */
+
+int print_last_digit(int n)
+{
+	char c;
+	int r;
+
+	r = _abs(n) % 10;
+	_putchar(r + '0');
+	return (r);
+
+}
