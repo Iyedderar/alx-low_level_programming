0b4cf4291b947e4af865c3fc557852740262f8c8
Iyedderar <iyedderar@yahoo.com>
2023-08-17 09:11:30 -0700

    FML

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,10 +1,11 @@
-#include"main.h"
-/**
- * _isupper - checks for uppercase characters
- * Return : 1 if uppercase 0 otherwise
- * @c character to check
- */
+#include "holberton.h"
 
+/**
+ * _isupper - checks for uppercase character.
+ * @c: the character to be checked.
+ *
+ * Return: 1 if uppercase otherwise 0
+ */
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
