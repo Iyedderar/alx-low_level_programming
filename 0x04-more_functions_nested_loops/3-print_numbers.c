992d9d9c6cd202178116fa514d262f99a483319a
Iyedderar <iyedderar@yahoo.com>
2023-08-17 09:29:24 -0700

    FML

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include"main.h"
+/**
+ * print_numbers - prints numbers
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int a = '0';
+
+	for ( ; a <= '9'; a++)
+		_putchar(a);
+	_putchar('\n')
+}
+
