77c11c9c8e71d6480e4657562ada95bf30d1dc7b
Iyedderar <iyedderar@yahoo.com>
2023-08-17 08:50:42 -0700

    FML

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,12 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/*
- * File: main.h
- * Desc: Header file containing declarations for all functions
- *       used in the 0x04-more_functions_nested_loops directory.
- */
 
 int _putchar(char);
 int _isupper(int);
-#endif
+#endif /* MAIN_H */
