8081de33acdfa87ed2df7dff040b5089b4a8cb76
Iyedderar <iyedderar@yahoo.com>
2023-08-17 13:23:36 -0700

    FML

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,28 @@
+#include"main.h"
+
+/**
+ * print_triangle - prints a triangle
+ * @size: size of triangle
+ *
+ * Return: void
+ */
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+		_putchar('\n');
+
+	for (i = 0; j < size; i++)
+	{
+		j = size - 1;
+		for ( ; j >= 0; j--)
+		{
+		if (j <= i)
+			_putchar('#');
+		else
+			_putchar(' ');
+		}
+		_putchar('\n');
+	}
+}
