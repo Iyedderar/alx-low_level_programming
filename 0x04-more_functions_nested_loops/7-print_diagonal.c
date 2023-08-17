b36f535df02bd357d7be5f90cbea3587ac25a1b2
Iyedderar <iyedderar@yahoo.com>
2023-08-17 10:48:43 -0700

    FML

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,23 @@
+#include"main.h"
+/**
+ * print_diagonal - prints numbers
+ * Return: void
+ * @n: number of lines
+ */
+
+void print_diagonal(int n)
+{
+	int i = 0;
+
+	int j = 0;
+
+	for ( ; i < n; i++)
+	{
+		j = 0;
+		for ( ; j < i; j++)
+			_putchar(' ');
+		_putchar('\\');
+		_putchar('\n');
+	}
+}
+
