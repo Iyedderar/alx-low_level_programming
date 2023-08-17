bafaf5b8e46d005f6067b5f519d4eb32eaa43b1d
Iyedderar <iyedderar@yahoo.com>
2023-08-17 09:19:01 -0700

    FML

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include"main.h"
+/**
+ * _isdigit - checks for uppercase characters
+ * Return: the product of the two argument
+ * @c: first operand
+ * @d: second operand
+ */
+
+int mul(int c, int d)
+{
+	return (c*d);
+}
+
