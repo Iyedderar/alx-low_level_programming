f992a88925e024da50be4fef339bc3aab0978a21
Iyedderar <iyedderar@yahoo.com>
2023-08-21 15:27:44 -0700

    FML

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,10 +11,10 @@ char *_strcpy(char *dest, const char *src)
 {
 	int i = 0;
 
-	while (src[index])
+	while (src[i])
 	{
-		dest[index] = src[index];
-		index++;
+		dest[i] = src[i];
+		i++;
 	}
 
 	return (dest);
