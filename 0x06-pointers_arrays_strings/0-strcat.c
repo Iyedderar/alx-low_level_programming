291b81ced27469980f840334d520241d7b58528a
Iyedderar <Iyedderar@yahoo.com>
2023-08-24 22:14:34 +0100

    FML

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *_strcat - concatenates  two strings
+ *@dest: destination string
+ *@src: source string
+ *
+ * Return: returns poiner to dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+
+	int i = 0, n = 0;
+
+	while (dest[i++])
+		n++;
+
+	for (i = 0; src[i]; i++)
+	{
+		dest[n] = src[i];
+		n++
+	}
+	dest[n] = '\0';
+	return (dest);
+}
