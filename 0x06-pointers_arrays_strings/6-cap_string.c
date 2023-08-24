25956af8bc8347a0b3015f33fc13dc681f70ec36
Iyedderar <Iyedderar@yahoo.com>
2023-08-25 00:11:43 +0100

    FML

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,31 +8,24 @@
 
 char *cap_string(char *str)
 {
-	int i = 0;
+int i = 0, j = 0;
+	int po[] = {' ', '\t', '\n', 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
+	if (str[i] >= 'a' && s[i] <= 'z')
+		str[i] -= 32;
+	i++;
 	while (str[i])
 	{
-		while (!(str[i] >= 'a' && str[i] <= 'z'))
-			i++;
-
-		if (str[i - 1] == ' ' ||
-		    str[i - 1] == '\t' ||
-		    str[i - 1] == '\n' ||
-		    str[i - 1] == ',' ||
-		    str[i - 1] == ';' ||
-		    str[i - 1] == '.' ||
-		    str[i - 1] == '!' ||
-		    str[i - 1] == '?' ||
-		    str[i - 1] == '"' ||
-		    str[i - 1] == '(' ||
-		    str[i - 1] == ')' ||
-		    str[i - 1] == '{' ||
-		    str[i - 1] == '}' ||
-		    i == 0)
-			str[i] -= 32;
-
+		for (; j < 13; j++)
+		{
+			if (str[i] == po[j])
+			{
+				if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+					str[i + 1] -= 32;
+				break;
+			}
+		}
 		i++;
 	}
-
 	return (str);
 }
