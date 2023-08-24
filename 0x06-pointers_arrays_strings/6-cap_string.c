32049a15c881b7ce9606e15e6841104ef2a40c53
Iyedderar <Iyedderar@yahoo.com>
2023-08-24 23:35:45 +0100

    FML

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,35 @@
+#include"main.h"
+
+/**
+ *cap_string - capitalizes words of str
+ *@str: string to be capitalized
+ *Return: ptr to str
+*/
+
+char *cap_string(char *str)
+{
+	int i = 0;
+
+	*str -= 32;
+	str++;
+	for ( ; *str; str++)
+	{
+		while (!(*str >= 'a' && *str <= 'z') && *str)
+			str++;
+		if (*(str - 1) == ' ' ||
+		    *(str - 1) == '\t' ||
+		    *(str - 1) == '\n' ||
+		    *(str - 1) == ',' ||
+		    *(str - 1) == ';' ||
+		    *(str - 1) == '.' ||
+		    *(str - 1) == '!' ||
+		    *(str - 1) == '?' ||
+		    *(str - 1) == '"' ||
+		    *(str - 1) == '(' ||
+		    *(str - 1) == ')' ||
+		    *(str - 1) == '{' ||
+		    *(str - 1) == '}')
+			*str -= 32;
+	}
+	return (str);
+}
