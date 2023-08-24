bb9e22bdaa7b8894694d5348e3b7a621056b161b
Iyedderar <Iyedderar@yahoo.com>
2023-08-24 23:39:31 +0100

    FML

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,26 +8,31 @@
 
 char *cap_string(char *str)
 {
-	*str -= 32;
-	str++;
-	for ( ; *str; str++)
+	int i = 0;
+
+	while (str[i])
 	{
-		while (!(*str >= 'a' && *str <= 'z') && *str)
-			str++;
-		if (*(str - 1) == ' ' ||
-		    *(str - 1) == '\t' ||
-		    *(str - 1) == '\n' ||
-		    *(str - 1) == ',' ||
-		    *(str - 1) == ';' ||
-		    *(str - 1) == '.' ||
-		    *(str - 1) == '!' ||
-		    *(str - 1) == '?' ||
-		    *(str - 1) == '"' ||
-		    *(str - 1) == '(' ||
-		    *(str - 1) == ')' ||
-		    *(str - 1) == '{' ||
-		    *(str - 1) == '}')
-			*str -= 32;
+		while (!(str[i] >= 'a' && str[i] <= 'z'))
+			i++;
+
+		if (str[i - 1] == ' ' ||
+		    str[i - 1] == '\t' ||
+		    str[i - 1] == '\n' ||
+		    str[i - 1] == ',' ||
+		    str[i - 1] == ';' ||
+		    str[i - 1] == '.' ||
+		    str[i - 1] == '!' ||
+		    str[i - 1] == '?' ||
+		    str[i - 1] == '"' ||
+		    str[i - 1] == '(' ||
+		    str[i - 1] == ')' ||
+		    str[i - 1] == '{' ||
+		    str[i - 1] == '}' ||
+		    i == 0)
+			str[i] -= 32;
+
+		i++;
 	}
+
 	return (str);
 }
