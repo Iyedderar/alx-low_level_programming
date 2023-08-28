2d070806e5648e0a1ebb98a1cfad8cff105483fe
Iyedderar <Iyedderar@yahoo.com>
2023-08-28 18:24:12 +0100

    FML

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -16,13 +16,13 @@ unsigned int _strspn(char *s, char *accept)
 	{
 		for (i = 0; accept[i]; i++)
 		{
-			if (accept[i] == '\0')
-				return (n);
 			if (*s == accept[i])
 			{
 				n++;
 				break;
 			}
+			if (accept[i+1] == '\0')
+				return (n);
 		}
 		s++;
 	}
