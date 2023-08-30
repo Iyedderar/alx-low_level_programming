db94f110876b023a77e2f0c0bd2d26d93dbc95d0
Iyedderar <Iyedderar@yahoo.com>
2023-08-30 20:31:22 +0100

    FML

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * prime_helper - Finds the natural square root of an inputted number.
+ * @n: The number to be checked.
+ * @s: The divisor
+ *
+ * Return: If the number has a natural square root - the square root.
+ *         If the number does not have a natural square root - -1.
+ */
+int prime_helper(int n, int s)
+{
+	if (n % s == 0)
+		return (0);
+
+	if (s == n / 2)
+		return (-1);
+
+	return (prime_helper(n, s + 1));
+}
+
+/**
+ * is_prime  -  prime function recursion
+ * @n: integer
+ * Return: the result
+ */
+
+int is_prime(int n)
+{
+	int s = 0;
+
+	if (n < 0)
+		return (-1);
+
+	if (n == 2 || n == 3)
+		return (1);
+
+	return (prime_helper(n, s));
+}
