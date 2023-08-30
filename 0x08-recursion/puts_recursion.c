5462b6c62a9b19ff9546089ce55e25fdbb23ecc4
Iyedderar <Iyedderar@yahoo.com>
2023-08-30 17:05:54 +0100

    FML

---
+++ 0x08-recursion/puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts_recursion -  implementation of puts function using recursion
+ * @s: source string
+ * Return:does not return nthn
+ */
+
+void _puts_recursion(char *s)
+{
+	if (s == '\0')
+		return;
+	_putchar(s);
+	_puts_recursion(s++);
+}
