9b5fd3a3ae6302917e06880c329df8a45a4e5eca
Iyedderar <Iyedderar@yahoo.com>
2023-09-07 10:43:29 +0100

    FML

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,54 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int _strlen(char *s);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+
+/**
+ * _strlen - finds the length of a string
+ * @s: the string
+ *
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int n = 0, i;
+
+	for(i = 0; s[i]; i++)
+		n++;
+	return (n);
+}
+
+/**
+ * string_nconcat -  function that concatenates two strings.
+ * @s1: first string.
+ * @s2: scnd string.
+ * @n: nbr of strings from s2
+ * Return:  pointer to the memory allocated
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *f;
+	unsigned int sz1, sz2, i;
+	
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	sz1 = _strlen(s1);
+	sz2 = _strlen(s2);
+	
+	if (n >= sz2)
+		n = sz2;
+	f = malloc(sizeof(char) * (sz1 + n + 1));
+	
+	if (f == NULL)
+		return (NULL);
+	for (i = 0; s1[i]; i++)
+		f[i] = s1[i];
+	for (i = 0; i < n; i++)
+		f[i + sz1] = s2[i];
+	f[sz1 + n] = '\0';
+	return f;	
+}
