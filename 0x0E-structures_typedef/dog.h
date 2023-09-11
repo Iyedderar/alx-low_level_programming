6d0f48ca223a5ae5f33256d6920bc896d997df84
Iyedderar <Iyedderar@yahoo.com>
2023-09-11 10:29:20 +0100

    FML

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#include <stdio.h>
-#include <stdlib.h>
+#ifndef DOG_H
+#define DOG_H
 
 /**
  * struct my_struct - Short description
@@ -15,3 +15,4 @@ struct dog
 	int age;
 	char *owner;
 };
+#endif
