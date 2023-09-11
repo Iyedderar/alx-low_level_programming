24bf602185ad5589a8a6eaaade6b8cd23e1b9f70
Iyedderar <Iyedderar@yahoo.com>
2023-09-11 15:35:17 +0100

    FML

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,28 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * print_dog - a function to print dog infos .
+ * @d: the dog variable.
+ */
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+
+		if (d->name == NULL)
+			printf("Name: (nil)\n");
+		else
+			printf("Name: %s\n", d->name);
+
+		if (d->age < 0)
+			printf("Age: (nil)\n");
+		else
+			printf("Age: %f\n", d->age);
+
+		if (d->owner == NULL)
+			printf("Owner: (nil)\n");
+		else
+			printf("Owner: %s\n", d->owner);
+	}
+}
