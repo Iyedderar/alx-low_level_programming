ad49400a996893c275565790e72da92a20fd95bd
Iyedderar <Iyedderar@yahoo.com>
2023-09-13 13:54:54 +0100

    FML

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,24 @@
+#include "function_pointers.h"
+
+/**
+* int_index - search function.
+* @size: size of the array.
+* @cmp: function to be applied.
+* @array: array to be processed
+* Return: return the index of element or -1
+*/
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0 || array == NULL || cmp == NULL)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+			return (i);
+	}
+	return (-1);
+}
