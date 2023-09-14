91015ccdc8287f640139010242ac1b0de8b69cb8
Iyedderar <Iyedderar@yahoo.com>
2023-09-14 10:44:31 +0100

    FML

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -12,9 +12,6 @@ int sum_them_all(const unsigned int n, ...)
 	va_list f;
 	unsigned int i, sum = 0;
 
-	if (n == 0)
-		return (0);
-
 	va_start(f, n);
 	for (i = 0; i < n; i++)
 		sum += va_arg(f, int);
