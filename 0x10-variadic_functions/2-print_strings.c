033f41169e87ce3e5e8ec2b556f83b4350b43339
Iyedderar <Iyedderar@yahoo.com>
2023-09-14 17:57:17 +0100

    FML

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -2,30 +2,6 @@
 #include <stdarg.h>
 #include <stdio.h>
 
-void print_strings(const char *separator, const unsigned int n, ...);
-char *_strcpy(char *dest, char *src);
-
-
-/**
- * _strcpy - copies a string from src to dest
- * @dest: destination
- * @src: source
- *
- * Return: pointer to char dest
- */
-char *_strcpy(char *dest, char *src)
-{
-	int i = 0;
-
-	while (src[i])
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	dest[i] = '\0';
-	return (dest);
-}
-
 
 /**
  * print_strings - Prints strs separated.
@@ -51,7 +27,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 	{
-		_strcpy(s, va_arg(f, char *));
+		s = va_arg(f, char *);
 		if (s == NULL)
 			printf("(nil)");
 		else
