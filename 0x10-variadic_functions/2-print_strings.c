8116ff421d52303125969831b8731dd5bb7e6c22
Iyedderar <Iyedderar@yahoo.com>
2023-09-14 17:59:23 +0100

    FML

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -32,7 +32,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			printf("(nil)");
 		else
 			printf("%s", s);
-		if (index != (n - 1))
+		if (i != (n - 1))
 		printf("%s", separator);
 	}
 	printf("\n");
