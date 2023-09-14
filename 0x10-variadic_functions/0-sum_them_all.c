8ee88291dc6d2a77080f270f97c229ca31a0f879
Iyedderar <Iyedderar@yahoo.com>
2023-09-14 10:34:50 +0100

    FML

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,7 +1,7 @@
 #include "variadic_functions.h"
 /**
  * sum_them_all - function to sum all arguments supplied.
- * @n: mandatory arguments.
+ * @n: nbr of arguments.
  * @'...': optional arguments.
  * Return: the sum .
  */
@@ -15,7 +15,8 @@ int sum_them_all(const unsigned int n, ...)
 		return (0);
 
 	va_start(f, n);
-	for (i = n; f != NULL ; i = va_arg(f, const unsigned int))
-		sum += i;
+	for (i = 0; i < n; i++)
+		sum += va_arg(f, const int);
+	va_end(f);
 	return (sum);
 }
