49259e8c3d5e1fb3b62a1864ba963038281d2cb8
iyedderar@yahoo.com <iyedderar@yahoo.com>
2023-09-28 17:22:26 +0100

    fml

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * clear_bit - sets the value of a bit at a given index.
+ * @n:pointer to The bit.
+ * @index: The index to get the value at - indices start at 0.
+ *
+ * Return: If an error occurs - -1.
+ *         Otherwise - sets the bit to 0.
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= (sizeof(unsigned long int) * 8) || n == NULL)
+		return (-1);
+
+	*n = *n | (0 << index);
+	return (1);
+}
