7e65a558dc58a0ab019abf9fe869f50ee56c6fc5
iyedderar@yahoo.com <iyedderar@yahoo.com>
2023-10-02 22:36:04 +0100

    fml

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -3,8 +3,8 @@
  * create_file - create a file.
  * @text_content: the content of the file .
  * @filename : the name of the file.
- * 
- * 
+ *
+ *
  * Return: 1 on success -1 on failure
  */
 int create_file(const char *filename, char *text_content)
@@ -15,7 +15,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content != NULL)
 	{
-		for (len; text_content[len];)
+		for (; text_content[len];)
 			len++;
 	}
 	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
