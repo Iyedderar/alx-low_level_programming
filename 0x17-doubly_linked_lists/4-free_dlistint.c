7cbe7f5f83d53aacbe6e7c50b0e5b7000dfec896
Iyedderar <Iyedderar@yahoo.com>
2023-11-09 12:13:47 +0100

    fml

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - doubly linked free.
+ * @head: pointer to pointer to the head.
+ *
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp = head;
+
+	while (head)
+	{
+		head = head->next;
+		free(temp);
+	}
+}
