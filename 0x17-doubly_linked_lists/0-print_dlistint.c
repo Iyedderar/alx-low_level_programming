d255668b5fef940bee54ef5d64942dac0e349f0b
Iyedderar <Iyedderar@yahoo.com>
2023-11-09 11:28:20 +0100

    fml

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -3,23 +3,18 @@
  * print_dlistint - doubly linked list print
  * @h: the head of the linked list
  *
- * Return: nbr of nodes in a linked list 
+ * Return: nbr of nodes in a linked list.
  */
 
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t size;
-	dlistint_t *temp = h;
 
-
-	if (h == NULL)
-		return (0);
-	
-	while (temp != NULL)
+	while (h != NULL)
 	{
-		printf("%d",temp->n);
+		printf("%d", h->n);
 		size++;
-		temp = temp->next;
+		h = h->next;
 	}
 	return (size);
 }
